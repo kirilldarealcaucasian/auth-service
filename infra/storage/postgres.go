5a2dbe5fdd62907b7030de9577cb5c3fd47ea620
kirill <tatarinov@apsolutions.ru>
2024-12-06 13:58:34 +0300

    docker-compose

--- infra/storage/postgres.go
+++ infra/storage/postgres.go
@@ -10,7 +10,31 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
+func setUpDBData(con *sqlx.DB) error {
+	q := `
+	CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
+	CREATE TABLE IF NOT EXISTS users (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    email VARCHAR(255) UNIQUE NOT NULL);
+
+	CREATE TABLE IF NOT EXISTS users_auth_info (
+				id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+				user_guid UUID,
+				refresh_token_hash VARCHAR,
+				ip_address VARCHAR NOT NULL,
+			  UNIQUE (user_guid, refresh_token_hash),
+				FOREIGN KEY (user_guid) REFERENCES users(id) ON DELETE CASCADE);
+	
+	INSERT INTO users (email) VALUES ('test@gmail.com') ON CONFLICT (email) DO NOTHING;
+				
+				`
+	_, err := con.Exec(q)
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
 
 func MustStorageInit(cfg *config.Config, logger *slog.Logger) *sqlx.DB {
@@ -26,5 +50,11 @@ func MustStorageInit(cfg *config.Config, logger *slog.Logger) *sqlx.DB {
     }
 		
   logger.Info("Successfully connected to db")
+
+	err = setUpDBData(db)
+	if err != nil {
+		panic(err)
+	}
+	logger.Info("Tables have been created")
 	return db
 }
--- setup.sql
+++ setup.sql
@@ -1,6 +1,8 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    email VARCHAR(255) NOT NULL
+    email VARCHAR(255) UNIQUE NOT NULL
 );
 
 CREATE TABLE users_auth_info (
